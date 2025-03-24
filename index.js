60171190ca013caa5be5c6a7239693711ff26784
Fabio dos Santos <fabio.santos@aiqia.com.br>
2025-03-24 15:18:43 -0300

    Finish implements of code and phone

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 
-app.get('/registers', repository.getRegisters)
+app.post('/validatecode', repository.getValidateCode)
 app.post('/registers', repository.postRegisters)
 
 app.listen(3000, () => console.log('Server running on port 3000'));
--- repositories/index.js
+++ repositories/index.js
@@ -3,24 +3,36 @@ const { pool } = require("../db")
 
 const generateCode = () => Math.floor(100000 + Math.random() * 900000).toString();
 
-const getRegisters = async (request, response) => {
+const getValidateCode = async (request, response) => {
     const { phone, code } = request.body;
-    try {
-        await pool.query('SELECT r.id, r.phone, c.code, c.created_at FROM registers AS r ' +
-            'JOIN codes AS c ' + 
-            'ON r.id = c.register_id WHERE r.phone = ' + phone + 
-            ' AND c.code = '+ code +'', 
-            (error, results) => {
-            if (error) {
-                throw error;
-            }
-            response.status(200).json({ success: 'ok'});
-        })
-    } catch (e) {
-        console.error('Error ao buscar code e telefone: ', error);
-        response.status(500).json({ error: 'Código não é mais válido'});
+
+    if(!phone || !code) {
+        return response.status(400).json({error: false});
+    } else {
+        try {
+            const query = `SELECT r.id, r.phone, c.code, c.created_at 
+            FROM registers AS r 
+            JOIN codes AS c ON r.id = c.register_id 
+            WHERE r.phone = $1 AND c.code = $2`;
+            const values = [phone, code]
+            await pool.query(query, values, 
+                (error, results) => {
+                if (error) {
+                    console.error('Erro ao validar code e telefone: ', error);
+                    return response.status(500).json({ error: 'Erro interno do servidor.' });
+                }
+                if (results.rows.length > 0) {
+                    response.status(200).json({ success: true });
+                } else {
+                    response.status(404).json({ error: false });
+                }
+            })
+        } catch (e) {
+            response.status(400).json({ error: false});
+            console.error('Error ao buscar code e telefone: ', error);
+            response.status(500).json({ error: 'Código não é mais válido'});
+        }
     }
-    
 }
 
 const postRegisters = async (request, response) => {
@@ -28,32 +40,55 @@ const postRegisters = async (request, response) => {
     if(!phone) return response.status(400).json({ error: 'Número de telefone obrigatório'});
 
     try {
-        const registerResult = await pool.query(
-            'INSERT INTO registers (phone) VALUES ($1) RETURNING id',
-            [phone]
+        await pool.query('BEGIN');
+        const existingRegisterResult = await pool.query('SELECT id FROM registers WHERE phone = $1', [phone]);
+        let registerId;
+
+        if(existingRegisterResult.rows.length > 0) {
+            registerId = existingRegisterResult.rows[0].id;
+            console.log('Telefone já cadastrado, register id: ' + registerId);
+        } else {
+            const newRegisterResult = await pool.query(
+                'INSERT INTO registers (phone) VALUES ($1) RETURNING id', [phone]
+            );
+            registerId = newRegisterResult.rows[0].id;
+            console.log('Telefone novo cadastrado, register id: ' + registerId);
+        }
+
+        const code = generateCode();
+        const existingCodeResult = await pool.query(
+            'SELECT id FROM codes WHERE register_id = $1', [registerId]
         );
-        const registerId = registerResult.rows[0].id;
-        const code = generateCode()
-        await pool.query(
-            'INSERT INTO codes (code, register_id) VALUES ($1, $2)',
-            [code, registerId]
-        )
+
+        if (existingCodeResult.rows.length > 0) {
+            await pool.query('UPDATE codes SET code = $1 WHERE register_id = $2', [code, registerId]);
+            console.log('Código atualizado para ser enviado');
+        } else {
+            await pool.query('INSERT INTO codes (code, register_id) VALUES ($1, $2)', [code, registerId]);
+            console.log('Código novo criado para ser enviado');
+        }
 
         const formData = new FormData();
         formData.append('groups', '["' + phone + '@s.whatsapp.net"]');
         formData.append('message', code);
-            console.log('Generated code: ', phone, code);
-            await axios.post('https://aiqia-back-js-whatsappapi.rj.r.appspot.com/send-message/cargomaps', formData, {
-                    headers: { 'Content-Type': 'multipart/form-data' },
-                });
-            response.json({ success: true, message: 'Código enviado com sucesso'});
-        } catch (error) {
-            console.error('Error ao registrar telefone: ', error);
-            response.status(500).json({ error: 'Erro interno no servidor'});
-        }
+        console.log('Generated code: ', phone, code);
+        await axios.post('https://aiqia-back-js-whatsappapi.rj.r.appspot.com/send-message/cargomaps', formData, {
+                headers: { 'Content-Type': 'multipart/form-data' },
+            });
+
+        await pool.query('COMMIT');
+        response.json({ 
+            success: true, 
+            message: 'Código enviado com sucesso',
+            code: code
+        });
+    } catch (error) {
+        console.error('Error ao registrar telefone: ', error);
+        response.status(500).json({ error: 'Erro interno no servidor'});
+    }
 }
 
 module.exports = {
-    getRegisters,
+    getValidateCode,
     postRegisters
 }
